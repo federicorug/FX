d160ff4844c61341ccdf8480a0b5cc3c4db67c96
federico <ruggefede98@gmail.com>
2025-07-03 17:50:04 +0200

    commit digitale

--- FxDerivative.py
+++ FxDerivative.py
@@ -159,5 +159,82 @@ class BarOption(Derivative):
         return self.fx_option.thetaPerDay()
 
 
+  
+class DigOption(Derivative):
+
+    def contract(self, option_type, strike, maturity_date, N):
+
+        self.ref = self.mkt.spot
+        self.domestic_curve = self.mkt.estr.rate_curve
+        self.foreign_curve = self.mkt.sofr.rate_curve
+        self.vol_ts = self.mkt.vol_handle
+        self.maturity_date = maturity_date
+        self.N = N
+        self.T  = (maturity_date - self.mkt.today).days / 360
+        self.strike = strike
+        self.option_type = option_type
+
+
+        if self.option_type == 'call':
+            self.option_type = Option.Call
+
+
+        elif self.option_type == 'put':
+            self.option_type = Option.Put
+
+
+        # spot_handle = QuoteHandle(SimpleQuote(self.ref))
+        # gk_process = GarmanKohlagenProcess(spot_handle, self.domestic_curve, self.foreign_curve, self.vol_ts)
+
+        # payoff = PlainVanillaPayoff(self.option_type, self.strike)
+        # self.fx_option = BarrierOption(
+        #     self.BarrierType,   
+        #     self.Barrier,
+        #     0.0,
+        #     payoff,
+        #     EuropeanExercise(Date(self.maturity_date.day, self.maturity_date.month, self.maturity_date.year))
+        # )
+
+        # engine = AnalyticBarrierEngine(gk_process)
+        # self.fx_option.setPricingEngine(engine)
+
+        payoff = CashOrNothingPayoff(self.option_type, self.strike, self.N)
+        exercise = EuropeanExercise(Date(self.maturity_date.day, self.maturity_date.month, self.maturity_date.year))
+
+        self.fx_option = EuropeanOption(payoff, exercise)
+
+        spot_handle = QuoteHandle(SimpleQuote(self.ref))
+
+        bsm_process = GarmanKohlagenProcess(spot_handle, self.domestic_curve, self.foreign_curve, self.vol_ts)
+
+        engine = AnalyticEuropeanEngine(bsm_process)
+        self.fx_option.setPricingEngine(engine)
+ 
+
+    def Premium_EUR(self):
+
+        npv = self.fx_option.NPV()
+
+        return npv/self.ref
+    
+    def delta_EUR(self):
+        
+        return self.fx_option.delta() * self.N / self.strike
+    
+    def gamma(self):
+
+        return self.fx_option.gamma()
+
+    def vega(self):
+
+        return self.fx_option.vega()
+
+    def theta_EUR(self):
+
+        return self.fx_option.thetaPerDay()
+
+
+
+
 
 
--- testpy.py
+++ testpy.py
@@ -1,16 +1,21 @@
 from FxDerivative import Vanilla, BarOption
 import datetime as dt
 
-option = BarOption()
+vanilla = Vanilla()
+vanilla.download_data()
+barriera = BarOption()
+barriera.mkt = vanilla.mkt
+
 
 strike = 1.2
-barrier = 1.23
+barrier = 1.3
 barrier_type = 'knock-in'
 date = dt.date(2026, 7, 3)
 option_type = 'call'
 N = 100000
 
+vanilla.contract(option_type, strike, date, N)
+barriera.contract(option_type, strike, barrier_type, barrier, date, N)
 
-option.contract(option_type, strike, barrier_type, barrier, date, N)
-
-print('Ref:', option.ref, 'option price:', round(option.Premium_EUR(),2), 'EUR')
+print('Ref:', vanilla.ref, 'option price:', round(vanilla.Premium_EUR(),2), 'EUR')
+print('Ref:', barriera.ref, 'option price:', round(barriera.Premium_EUR(),2), 'EUR')
